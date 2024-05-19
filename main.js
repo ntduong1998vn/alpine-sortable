2522dab3d5fe423671b0f8f46ff757b2573e35a8
ntduong1998vn <ntduong1998vn@gmail.com>
2024-05-19 15:08:38 +0700

    init select2

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import './style.css'
 
-import Sortable from "sortablejs";
+import jQuery from "jquery"
+
+window.$ = window.jQuery = jQuery
+import select2 from "select2/dist/js/select2.min.js";
+import "select2/dist/css/select2.min.css" 
+import "select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css"
+select2()
+
+
 import Alpine from "alpinejs";
-import sort from "@alpinejs/sort";
-
 window.Alpine = Alpine
-
 Alpine.start()
+
