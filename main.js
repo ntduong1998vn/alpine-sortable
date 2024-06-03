240010e1568753437ce2718f40a67ae945a9a3d7
ntduong1998vn <ntduong1998vn@gmail.com>
2024-06-03 15:29:48 +0700

    save

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import jQuery from "jquery"
 window.$ = window.jQuery = jQuery
 import select2 from "select2/dist/js/select2.min.js";
 import "select2/dist/css/select2.min.css" 
-// import "select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css"
+import "select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css"
 select2()
 
 
@@ -15,4 +15,3 @@ import sort from '@alpinejs/sort'
 Alpine.plugin(sort)
 window.Alpine = Alpine
 Alpine.start()
-
